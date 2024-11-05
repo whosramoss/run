38308ad50e9e89ede35fdbf066771df6613d9184
Gabriel Ramos <whosramoss@gmail.com>
2024-11-05 10:39:37 -0300

    chore: delete files

--- src/client/components/Center.tsx
+++
@@ -1,7 +0,0 @@
-export default function Center({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-col items-center justify-center pb-[114px] pt-28 text-[30px] font-bold md:pb-[67px] md:text-[38px] lg:py-[144px] lg:pt-72">
-      {children}
-    </div>
-  );
-}
